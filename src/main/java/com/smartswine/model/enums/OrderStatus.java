884a2d337a6f1bc12c0e0e547c481869432af606
zawmyowin-w <winwin.ab89@gmail.com>
2026-06-25 22:52:44 +0630

    add src/main/java/com/smartswine/model/enums/OrderStatus.java

---
+++ src/main/java/com/smartswine/model/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.smartswine.model.enums;
+
+public enum OrderStatus {
+    PENDING,
+    STOCK_RESERVED,
+    PAYMENT_PENDING,
+    PAYMENT_VERIFIED,
+    CONFIRMED,
+    DELIVERING,
+    COMPLETED,
+    CANCELLED
+}
